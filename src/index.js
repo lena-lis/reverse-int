f28b61fa025c3102706695fd69c14f6d8862d075
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2024-08-12 21:02:24 +0300

    feat: add task's solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let num = (Math.abs(n)).toString();
+  let result = '';
+  for (let i = num.length - 1; i >= 0; i--) {
+    result += num[i];
+  }
+  return +result;
 }
